17d0b3a21a03a0f7154f313a30cbcd01c151996a
ishimwengambasabri@gmail.com <ishimwengambasabri@gmail.com>
2026-04-11 10:46:34 +0200

    project

---
+++ backend/controller/usercontroller.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const transporter = nodemailer.createTransport({
+  service: process.env.SERVICE,
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASSWORD
+  }
+});
+
+exports.sendEmail = async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const emailOptions = {
+    from: email,
+    to: process.env.RECIPIENT,
+    subject: `Message from ${name}`,
+    text: message,
+    replyTo: email
+  };
+
+  try {
+    const info = await transporter.sendMail(emailOptions);
+    console.log('Email sent:', info.response);
+
+    res.status(200).json({ message: "Email sent successfully" });
+
+  } catch (err) {
+    console.error('Error sending email:', err);
+
+    res.status(500).json({ message: "Failed to send email" });
+  }
+};
---
+++ backend/router/userrouter.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const router = express.Router();
+
+const { sendEmail } = require("../controller/usercontroller");
+
+// POST route for creating user
+
+router.post("/email", sendEmail);
+
+
+
+module.exports = router;
